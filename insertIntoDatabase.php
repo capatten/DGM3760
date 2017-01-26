626400534d84d7e3a6166c9e1c52bc35a7e1d6fe
capatten <pattenchada@gmail.com>
2017-01-25 17:27:58 -0700

    Added sql structure

--- insertIntoDatabase.php
+++ insertIntoDatabase.php
@@ -1,11 +1,23 @@
 <?php
 //Load PHP variables
-$fname = $_POST[emailFrm_FirstName];
-$lname = $_POST[emailFrm_LastName];
-$email = $_POST[emailFrm_EmailAddress];
-$emailRsn = $_POST[emailFrm_Reason];
-$agreedToConditions = $_POST[emailFrm_LastName];
+//set variables
+$emailFirstName = $_POST['emailFrm_FirstName'];
+$emailLastName = $_POST['emailFrm_LastName'];
+$emailAddress = $_POST['emailFrm_EmailAddress'];
+$emailReason = $_POST['emailFrm_Reason'];
+$emailAgreeToTerms_bool = $_POST['emailFrm_agreeToTerms'];
 
 //connect to the database
-$dbConnection = mysqli_connect('localhost','pattende_dgm3760','ZUn($4+3ULIc','pattende_dgm3760') or die ('connection failed');
+$dbConnection = mysqli_connect('localhost','pattende_dgm3760','Pass1word','pattende_dgm3760') or die('Test');
+
+//build query
+$query = 
+	"INSERT INTO Assignment2(f_name, l_name, email_address, email_reason, agreed_to_conditions)".
+	"VALUES('$emailFirstName','$emailLastName','$emailAddress','$emailReason','$emailAgreeToTerms_bool')";
+
+//run Query
+$result = mysqli_query($dbConnection, $query) or die ('Query Failed');
+
+//close connection
+mysqli_close($dbConnection);
 ?>
