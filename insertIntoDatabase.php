b5f703df7a035601304293c720fb7b3ddb6a4ea8
capatten <pattenchada@gmail.com>
2017-01-26 18:23:03 -0700

    fixed handling single quotes

--- insertIntoDatabase.php
+++ insertIntoDatabase.php
@@ -11,17 +11,30 @@ if(isset($_POST['emailFrm_agreeToTerms'])){
 	$emailAgreeToTerms_bool = '0';
 }
 
+/* settype to avoid SQL injection */
+settype($emailFirstName, ' varchar');
+settype($emailLastName, ' varchar');
+settype($emailAddress, ' varchar');
+settype($emailReason, ' varchar');
+settype($emailAgreeToTerms_bool, ' integer');
+
 
 //connect to the database
 $dbConnection = mysqli_connect('localhost','pattende_dgm3760','Pass1word','pattende_dgm3760') or die('Test');
 
+//escape variables
+$esc_emailFirstName = mysqli_real_escape_string($dbConnection, $emailFirstName);
+$esc_emailLastName = mysqli_real_escape_string($dbConnection, $emailLastName);
+$esc_emailAddress = mysqli_real_escape_string($dbConnection, $emailAddress);
+$esc_emailReason = mysqli_real_escape_string($dbConnection, $emailReason);
+
 //build query
 $query = 
 	"INSERT INTO Assignment2(f_name, l_name, email_address, email_reason, agreed_to_conditions)".
-	"VALUES('$emailFirstName','$emailLastName','$emailAddress','$emailReason','$emailAgreeToTerms_bool')";
+	"VALUES('$esc_emailFirstName','$esc_emailLastName','$esc_emailAddress','$esc_emailReason','$emailAgreeToTerms_bool')";
 
 //run Query
-//$result = mysqli_query($dbConnection, $query) or die ('Query Failed');
+$result = mysqli_query($dbConnection, $query) or die ('Query Failed');
 
 //close connection
 mysqli_close($dbConnection);
@@ -32,7 +45,8 @@ mysqli_close($dbConnection);
 	<title>Thank you</title>
 </head>
 <body>
-	<h2>Thank you <?php echo $emailFirstName. $emailLastName; ?>!</h2>
-	<p>Your email has been sent. We will be contacting you shortly!</p>
+	<h2>Thank you <?php echo $emailFirstName . ' ' . $emailLastName;?>!</h2>	
+	<p>We will be sending an email to you at <strong><?php echo $emailAddress;?></strong></p>
+	<p>The subject of the email will say "<?php echo $emailReason;?>"</p>
 </body>
 </html>
